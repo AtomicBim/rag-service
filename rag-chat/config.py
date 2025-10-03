cfd3b7ed9ff50cfbccb66825e30cac45ba054240
AtomicBim <romangman90@gmail.com>
2025-10-03 11:37:47 +0500

    Переименовал контейнер эмбеддинга

--- rag-chat/config.py
+++ rag-chat/config.py
@@ -3,5 +3,5 @@ QDRANT_PORT = 6333
 COLLECTION_NAME = "internal_regulations_v2"
 SEARCH_LIMIT = 30  # Лимит документов для проверки ответа
 
-EMBEDDING_SERVICE_ENDPOINT = "http://embedding-service:8001/create_embedding" 
+EMBEDDING_SERVICE_ENDPOINT = "http://rag-embedding:8001/create_embedding" 
 OPENAI_API_ENDPOINT = "http://rag-bot:8000/generate_answer" 
